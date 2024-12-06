e018c8782fe0e399a07dcb03dced71ada9d716b2
Gauthier Dandele <92022724+GogoVega@users.noreply.github.com>
2024-12-06 10:38:52 +0100

    Do not call signout if app initialization failed (#15)

--- src/lib/firebase/client/client.ts
+++ src/lib/firebase/client/client.ts
@@ -154,16 +154,16 @@ export class Client extends TypedEmitter<ClientEvents> {
 		if (this.signState === SignState.NOT_YET) throw new ClientError("signOut called before signIn call");
 		if (this.signState === SignState.SIGN_OUT) throw new ClientError("signOut already called");
 
-		// The app is created regardless of whether the client has been initialized
-		// If initialized sign out, otherwise skip
-		if (this._clientInitialised) {
+		// Ensure the app has been created to signout and delete it
+		if (this._app) {
 			this._signState = SignState.SIGN_OUT;
 			this.emit("sign-out");
 
-			if (!this.admin) await signOut(this._auth!);
-		}
+			// Admin SDK has no signOut method - internally called during deleteApp
+			if (!this.admin && this._auth) await signOut(this._auth);
 
-		return this.deleteClient();
+			return this.deleteClient();
+		}
 	}
 
 	private async wrapSignIn(config: AppOptions): Promise<void>;
@@ -181,13 +181,35 @@ export class Client extends TypedEmitter<ClientEvents> {
 		try {
 			this._signState = SignState.SIGN_IN;
 			this.emit("sign-in");
-			this._app = admin ? new AdminApp(config, this.appName) : new App(this.appConfig, this.appName);
-			this._auth = admin ? undefined : getAuth(this._app.app as FirebaseApp);
-			this._clientInitialised = true;
-			const user = await signInFn();
-			this._signState = SignState.SIGNED_IN;
-			success = true;
-			return user;
+
+			if (!admin) {
+				// Initialize the app
+				this._app = new App(this.appConfig, this.appName);
+
+				// Validate the API Key
+				this._auth = getAuth(this._app.app);
+
+				// The client is ready to init DB
+				this._clientInitialised = true;
+
+				// Starting sign in
+				const user = await signInFn();
+
+				// Signed in
+				this._signState = SignState.SIGNED_IN;
+				success = true;
+				return user;
+			} else {
+				// Initialize the app and sign in
+				this._app = new AdminApp(config, this.appName);
+
+				// The client is ready to init DB
+				this._clientInitialised = true;
+
+				// Signed in
+				this._signState = SignState.SIGNED_IN;
+				success = true;
+			}
 		} finally {
 			if (!success) this._signState = SignState.ERROR;
 			this.emit(success ? "signed-in" : "sign-in-error");
