2fd5b7adfaa62efb7765731b403d2c1e46946855
GogoVega <92022724+GogoVega@users.noreply.github.com>
2025-08-25 10:50:09 +0200

    Bump for 0.2.7

--- src/nodes/firebase-config.ts
+++ src/nodes/firebase-config.ts
@@ -19,7 +19,7 @@ import { getDatabaseSettings, updateDatabaseSettings } from "../lib/nodes/rtdb-s
 import { FirebaseClient } from "../lib/nodes/firebase-client";
 import { Config, ConfigNode } from "../lib/nodes/types";
 
-const VERSION = "0.2.6";
+const VERSION = "0.2.7";
 
 export default async function (RED: NodeAPI) {
 	/**
